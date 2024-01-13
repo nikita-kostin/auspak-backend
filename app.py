f5355eae909c27dff31adf26ff88d211d3c7fe7a
Maxim Solodukhin <maxim.solodukhin@tum.de>
2024-01-13 20:05:34 +0100

    Allow parcel stops, restrict passengers to one stop, dummy driver selection

--- app.py
+++ app.py
@@ -310,14 +310,37 @@ async def open_chat(websocket: WebSocket, chat_id: int, current_user: User = Dep
 
 # Define the endpoint for creating a stop
 @app.post("/stop")
-def create_stop(driver_id: int, address: str, coordinates: str, current_user: User = Depends(get_current_user)):
-    # Check if the current user is a passenger
-    if current_user.entity == UserEntity.passenger:
+def create_stop(coordinates: str, current_user: User = Depends(get_current_user)):
+    # Check if the current user is a passenger or a parcel operator
+    if current_user.entity == UserEntity.passenger or current_user.entity == UserEntity.parcel_operator:
+        if current_user.entity == UserEntity.passenger:
+            # Check if the passenger already requested a stop
+            # Query the stop table with user id
+            response = supabase.table("stops").select("*").eq("user_id", current_user.id).execute()
+            # Check if response has data
+            if response.data:
+                # Raise an exception if the stop is not possible
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail="Passengers can only request one stop at a time",
+                )
+        # Dummy driver selection: first driver in the table
+        # Query the user table to get all drivers
+        response = supabase.table("users").select("id").eq("entity", UserEntity.driver.value).execute()
+        # Check if response has data
+        if response.data:
+            driver_id = response.data[0]["id"]
+        else:
+            # Raise an exception if the stop is not possible
+            # TODO: change to another status code, e.g., 2xx
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Creating a stop is not possible",
+            )
         # Insert the stop data into the stop table
         response = supabase.table("stops").insert([{
             "driver_id": driver_id,
             "user_id": current_user.id,
-            "address": address,
             "coordinates": coordinates
         }]).execute()
         # Check if the response has data
@@ -334,7 +357,7 @@ def create_stop(driver_id: int, address: str, coordinates: str, current_user: Us
         # Raise an exception if the current user is not a passenger
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="Only passengers can create stops",
+            detail="Only passengers or parcel operators can create stops",
         )
 
 
