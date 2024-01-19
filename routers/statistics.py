7b551c66bebf4c98d82f8da8ca7bd881e34a64f0
Artem Melnychuk <melnychuk.letter@gmail.com>
2024-01-19 17:07:08 +0100

    Statistics

--- routers/statistics.py
+++ routers/statistics.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+
 
 from dependencies import get_current_user
-from models import User
-
+from models import User, supabase, UserEntity
 
 router = APIRouter(prefix="/statistics", tags=["statistics"])
 
@@ -10,4 +10,22 @@ router = APIRouter(prefix="/statistics", tags=["statistics"])
 # Define the endpoint for getting statistics
 @router.get("/")
 def get_statistics(current_user: User = Depends(get_current_user)):
-    return {"statistics": "TBD"}
+    if current_user.entity == UserEntity.parcel_operator:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only parcel operators can get statistics",
+        )
+        return {"data": {}}
+        #TODO len, but must be count to avoid loading all data
+    statistics = {
+        "parcels_delivered": len(supabase.table("stops").select("*").execute().data),
+        "parcels_pending": len(supabase.table("stops").select("*").eq("is_active", True).execute().data),
+        "peak_hours": "11:43",
+        "revenue_per_parcel": 9,
+        "avg_cost_per_parcel": 8,
+        "capacity_utilization": 80.8,
+        "emissions": 0.129,
+        "customer_retention": 73.4,
+        "parcels_damage": 0.13
+    }    
+    return {"data": statistics}
