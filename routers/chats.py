37c480d9a5824f9b43a27b35aac98916e5244e0e
Maxim Solodukhin <maxim.solodukhin@tum.de>
2024-01-23 14:56:48 +0100

    allow chats between everyone, show name

--- routers/chats.py
+++ routers/chats.py
@@ -14,52 +14,31 @@ router = APIRouter(prefix="/chats", tags=["chats"])
 # Define the endpoint for creating a chat
 @router.post("/")
 def create_chat(user_id: int, current_user: User = Depends(get_current_user)):
-    # Check if the current user is a driver
-    if current_user.entity == UserEntity.driver:
-        # Query the user table with the 2nd user id
-        response = supabase.table("users").select("entity").eq("id", user_id).execute()
-        # Check if response has data
-        if response.data:
-            # Check if the 2nd user is a driver
-            if response.data[0]["entity"] == UserEntity.driver.value:
-                # Raise an exception if the 2nd user is a driver
-                raise HTTPException(
-                    status_code=status.HTTP_403_FORBIDDEN,
-                    detail="Drivers cannot create chats with other drivers",
-                )
-        else:
-            # Raise an exception if user not found
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="No user with given id found"
-            )
-        # Check if the chat between the users already exists
-        # Query the chat table with driver and user id
-        response = supabase.table("chats").select("*").eq("driver_id", current_user.id).eq("user_id", user_id).execute()
-        # Check if response has data
-        if response.data:
-            # Return the existing chat id
-            return {"chat_id": response.data[0]["id"]}
-        # Insert the chat data into the chat table
-        response = supabase.table("chats").insert([{
-            "driver_id": current_user.id,
-            "user_id": user_id
-        }]).execute()
-        # Check if the response has data
-        if response.data:
-            # Return the chat id
-            return {"chat_id": response.data[0]["id"]}
-        else:
-            # Raise an exception if the chat creation failed
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Chat creation failed",
-            )
+    # Check if the chat between the users already exists
+    # Query the chat table with driver and user id
+    response = supabase.table("chats")\
+        .select("*")\
+        .in_("driver_id", [current_user.id, user_id])\
+        .in_("user_id", [current_user.id, user_id])\
+        .execute()
+    # Check if response has data
+    if response.data:
+        # Return the existing chat id
+        return {"chat_id": response.data[0]["id"]}
+    # Insert the chat data into the chat table
+    response = supabase.table("chats").insert([{
+        "driver_id": current_user.id,
+        "user_id": user_id
+    }]).execute()
+    # Check if the response has data
+    if response.data:
+        # Return the chat id
+        return {"chat_id": response.data[0]["id"]}
     else:
-        # Raise an exception if the current user is not a driver
+        # Raise an exception if the chat creation failed
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Only drivers can create chats",
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Chat creation failed",
         )
 
 
@@ -97,14 +76,19 @@ def get_chat_history(chat_id: int, current_user: User = Depends(get_current_user
     if response.data:
         # Get the chat as a Chat object
         chat = Chat(**response.data[0])
-        if current_user.id in [chat.driver_id, chat.user_id]:
-            response = supabase.table("messages").select("*").eq("chat_id", chat_id).execute()
-            return response.data
+        response = supabase.table("users").select("*")
+        if current_user.id == chat.driver_id:
+            response = response.eq("id", chat.user_id).execute()
+        elif current_user.id == chat.user_id:
+            response = response.eq("id", chat.driver_id).execute()
         else:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="User is not authorised to access the chat",
             )
+        full_name = f'{response.data[0]["first_name"]} {response.data[0]["last_name"]}'
+        response = supabase.table("messages").select("*").eq("chat_id", chat_id).execute()
+        return {"name" : full_name, "data" : response.data}
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
